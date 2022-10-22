a598767069bc008272666930feca537c4864540b
Hessam Imen <hessam.imen@gmail.com>
2022-10-22 15:00:20 +1100

    Components to Footer added to the project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,17 +8,17 @@ function About() {
                 <h2 className='text-5xl font-bold'>Trusted by developers across the world</h2>
                 <p className='text-3xl py-6 text-gray-500'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque eligendi fugit ipsam obcaecati? Pariatur necessitatibus deleniti, voluptatem iusto eveniet voluptatum nisi facere eius qui cum.</p>
             </div>
-            <div className='grid md:grid-cols-3 gap-1 px-2 text-center '>
+            <div className='grid md:grid-cols-3 gap-5 px-2 text-center '>
                 <div className='border p-2 rounded-xl shadow-xl'>
-                    <p className='text-5xl font-bold text-indigo-600'>100%</p>
+                    <p className='text-4xl font-bold text-indigo-600'>100%</p>
                     <p className='text-gray-400 mt-2'>Completion</p>
                 </div>
                 <div className='border p-2 rounded-xl shadow-xl'>
-                    <p className='text-5xl font-bold text-indigo-600'>24/7</p>
+                    <p className='text-4xl font-bold text-indigo-600'>24/7</p>
                     <p className='text-gray-400 mt-2'>Delivery</p>
                 </div>
                 <div className='border p-2 rounded-xl shadow-xl'>
-                    <p className='text-5xl font-bold text-indigo-600'>100k</p>
+                    <p className='text-4xl font-bold text-indigo-600'>100k</p>
                     <p className='text-gray-400 mt-2'>Transaction</p>
                 </div>
             </div>
